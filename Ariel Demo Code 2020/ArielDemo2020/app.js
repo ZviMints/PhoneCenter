1f141b8108d404ae9c57364b9f90f6ae849af5a8
ZviMints <zvimints@gmail.com>
2020-08-01 14:30:23 +0300

    added space

--- Ariel Demo Code 2020/ArielDemo2020/app.js
+++ Ariel Demo Code 2020/ArielDemo2020/app.js
@@ -8,6 +8,7 @@ const port = 3000
 const kafka = require('./kafkaProduce');
 const bodyParser = require('body-parser');
 
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
